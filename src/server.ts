6801c195ac3d403d5df11d4de818cfb94c19b2cf
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-22 16:18:44 -0300

    request with GET method - browser

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,8 @@ const PORT = 3333;
 //inicializa o express
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Hello World Express!");
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
