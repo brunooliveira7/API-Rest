e88dc3c3dddd991e1597f82f5dd1165cc838d4cb
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-22 17:18:35 -0300

    retrieves data in the request body, json format

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,21 @@ const PORT = 3333;
 //inicializa o express
 const app = express();
 
-//rota na raiz da aplicação
+//vai permitir que o express entenda o formato json
+app.use(express.json());
+
+//rota na raiz da aplicação - método get no browser
 app.get("/products", (request, response) => {
   //recuperar os parâmetros da requisição
   const { page, limit } = request.query;
-
   response.send(`Pagina ${page} de limite ${limit}`);
 });
+
+//rota para recuperar o corpo da requisição - insomnia, json no body
+app.post("/products", (request, response) => {
+  const { name, price } = request.body;
+  response.send(`Produto ${name} custa ${price}`);
+});
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
