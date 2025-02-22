bc951a853d2451fc3a891be4ecf08ca3b6293060
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-22 16:34:26 -0300

    recover route params

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,11 @@ const PORT = 3333;
 //inicializa o express
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World Express!");
+app.get("/products/:id/:user", (request, response) => {
+  //recupera o parâmetro id da rota - /products/1
+  const { id, user } = request.params;
+
+  response.send(`Produto ${id} do usuário ${user}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
