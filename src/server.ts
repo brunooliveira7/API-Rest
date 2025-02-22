87df2d03dfef9e19f8da9363167e066a0d6ae883
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-02-22 16:14:23 -0300

    initialize the API with Express

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,9 @@
 import express from "express";
 
+///porta que o servidor vai rodar
+const PORT = 3333;
+
+//inicializa o express
+const app = express();
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
